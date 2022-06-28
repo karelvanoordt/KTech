3a5b38f959a64bccf07f41e9f0195c0b1fb95165
Karel van Oordt <vanoordtkarel@gmail.com>
2022-06-28 17:49:21 -0500

    Add uploader for image handling

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+    mount_uploader :image, ImageUploader
+    serialize :image, JSON # In case of SQLite
+    belongs_to :user, optional: true
+
+    validates :item_name, :price, :condition, :description, presence: true
+    validates :description, length: { maximum: 1000, too_long: 'Description shoud be 1000 characters max' }
+    validates :item_name, length: { maximum: 140, too_long: 'Item name should be 140 characters max' }
+    validates :price, numericality: { only_integer: true }, length: { maximum: 7 }
+
+    CONDITION = %w{ New Excellent Mint Used Fair Poor }
 end
